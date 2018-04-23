b05bea175d7a51f2d53694300099e2bbd2749acb
Ragland <jakearagland@gmail.com>
2018-04-23 16:16:41 -0500

    still need to do css work

---
+++ main.js
@@ -0,0 +1,61 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAw5uTmmmJQUvY8gm6ODfRgtlV8e5nMPjA",
+    authDomain: "mywebsite-94e2b.firebaseapp.com",
+    databaseURL: "https://mywebsite-94e2b.firebaseio.com",
+    projectId: "mywebsite-94e2b",
+    storageBucket: "mywebsite-94e2b.appspot.com",
+    messagingSenderId: "813927513528"
+  };
+  firebase.initializeApp(config);
+  // Reference message collection
+
+  var messagesRef = firebase.database().ref('messages');
+
+// Listen for form submit
+
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+// Submit form
+function submitForm(e){
+    e.preventDefault();
+    
+// Get values
+var first = getImputVal('first');
+var last = getImputVal('last');
+var email = getImputVal('email');
+var company = getImputVal('company');
+var message = getImputVal('message');
+// Save message
+saveMessage(first, last, email, company, message);
+
+// Show aleart 
+document.querySelector('.alert').style.display= 'block';
+
+// Hide alert after 3 seconds
+setTimeout(function(){
+document.querySelector('.alert').style.display= 'none';
+},3000)
+
+//Clear form
+document.getElementById('contactForm').reset();
+
+}
+
+// Function to get from values
+
+function getImputVal(id){
+    return document.getElementById(id).value;
+}
+
+// Save message to firebase
+
+function saveMessage(first, last, email, company, message) {
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+        first: first,
+        last: last,
+        email: email,
+        company: company,
+        message: message
+    })
+}
